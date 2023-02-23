3c6cae1f5ed4e0751e9c851f1dfdbef6ff91e457
Leigh Hobson <leighhobson89@aol.com>
2023-02-23 20:13:08 +0100

    weather app with openweathermap api

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        WeatherDisplay weatherDisplay = new OpenWeatherMapDisplay();
-        String userInput = OpenWeatherMapDisplay.inputCity("Please enter a city to get its weather:");
-        weatherDisplay.displayCurrentWeatherForCity(userInput);
+        new OpenWeatherMapDisplay();
     }
 }
--- src/OpenWeatherMapDisplay.java
+++ src/OpenWeatherMapDisplay.java
@@ -1,14 +1,92 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
+
+import javax.swing.*;
+import javax.swing.border.Border;
 
 import org.json.JSONObject;
 
-public class OpenWeatherMapDisplay implements WeatherDisplay {
+public class OpenWeatherMapDisplay extends JFrame implements WeatherDisplay {
 
     private static final String API_KEY = "1e0cd5d76098e28a884b484fbaae1395";
+    private JTextField cityField;
+    private JLabel cityLabel;
+    private JLabel descriptionLabel;
+    private JLabel temperatureLabel;
+
+    public OpenWeatherMapDisplay() {
+        // Create UI components
+        JPanel inputPanel = new JPanel();
+        cityField = new JTextField(20);
+        inputPanel.add(new JLabel("Enter city name:"));
+        inputPanel.add(cityField);
+
+        JPanel displayPanel = new JPanel();
+        displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS));
+
+        cityLabel = new JLabel();
+        descriptionLabel = new JLabel();
+        temperatureLabel = new JLabel();
+
+        // Create an instance of EmptyBorder with a left padding of 10 pixels
+        Border paddingBorder = BorderFactory.createEmptyBorder(0, 43, 0, 0);
+        Border paddingBorderCity = BorderFactory.createEmptyBorder(20, 43, 0, 0);
+
+        // Add the paddingBorder to each JLabel in the displayPanel
+        cityLabel.setBorder(paddingBorderCity);
+        descriptionLabel.setBorder(paddingBorder);
+        temperatureLabel.setBorder(paddingBorder);
+
+        displayPanel.add(cityLabel);
+        displayPanel.add(descriptionLabel);
+        displayPanel.add(temperatureLabel);
+
+        JButton button = new JButton("Get Weather");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get weather data for the entered city
+                String city = cityField.getText();
+                displayCurrentWeatherForCity(city);
+            }
+        });
+
+        // Add a KeyListener to the cityField
+        cityField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // If the Enter key is pressed, simulate a button click
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    button.doClick();
+                }
+            }
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+        // Add components to the frame
+        getContentPane().add(inputPanel, BorderLayout.NORTH);
+        getContentPane().add(displayPanel, BorderLayout.CENTER);
+        getContentPane().add(button, BorderLayout.SOUTH);
+
+        // Set frame properties
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        setTitle("OpenWeatherMap Display");
+        setVisible(true);
+    }
+
     @Override
     public void displayCurrentWeatherForCity(String city) {
         String API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
@@ -35,27 +113,12 @@ public class OpenWeatherMapDisplay implements WeatherDisplay {
             String country = jsonResponse.getJSONObject("sys").getString("country");
 
             // Display current weather data
-            System.out.println("Current weather for " + city + ", " + country + ":");
-            System.out.println("Description: " + description);
-            System.out.println("Temperature: " + String.format("%.1f", (temperature - 273)) + "ºC");
+            cityLabel.setText(city + ", " + country);
+            descriptionLabel.setText("Weather: " + description);
+            temperatureLabel.setText("Temperature: " + String.format("%.1f", (temperature - 273)) + "ºC");
 
         } catch (Exception e) {
             System.err.println("Error retrieving current weather data for " + city + ": " + e.getMessage());
         }
     }
-
-    public static String inputCity(String prompt) {
-        // Create a new Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
-
-        // Print the prompt message
-        System.out.print(prompt + " ");
-
-        // Read the user's input as a string
-        String userInput = scanner.nextLine();
-
-        // Return the user's input
-        return userInput;
-    }
-
 }
